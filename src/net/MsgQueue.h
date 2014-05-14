d5f9d7a94a76de3ba153c31b186203093567f3e0
ichenq <ichenq@gmail.com>
2014-05-14 21:19:46 +0800

    网络部分代码

---
+++ src/net/MsgQueue.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <Windows.h>
+#include <deque>
+
+class CBaseMessage;
+typedef std::deque<CBaseMessage*> msgQueue;
+
+/* -------------------------------------------
+  [CMsgQueue] 消息队列
+
+  功能:
+	存储接收到的消息，等待处理。
+
+---------------------------------------------*/
+class CMsgQueue  
+{
+private:
+	msgQueue m_msgQueue;	// 队列
+	CRITICAL_SECTION m_CriticalSectionMsgQueue;
+
+public:
+	CMsgQueue();
+	virtual ~CMsgQueue();
+
+	bool PushMessage(CBaseMessage* pMsg);	// 压入消息
+	//压入多个消息到队列前
+	bool PushMsgsoFront(msgQueue& queMsgs);
+	CBaseMessage* PopMessage();				// 弹出消息
+	void	GetAllMessage(msgQueue& pTemptMsgQueue);
+
+	long GetSize();							// 得到消息队列长度
+	void Clear();							// 清空消息
+};
+
