5228bbb345bf5cf631e7b754bed4d641313d504d
ichenq <ichenq@gmail.com>
2014-05-14 21:20:44 +0800

    内存分配器

---
+++ src/io/DataBlock.cpp
@@ -0,0 +1,34 @@
+//=============================================================================
+/**
+ *  file: DataBlock.cpp
+ *
+ *  Brief:有固定大小的内存快，动态分配数据块的基本机构。
+ *
+ *  Authtor:wangqiao
+ *	
+ *	Date:2007-6-13
+ */
+//=============================================================================
+
+#include "DataBlock.h"
+#include "base/logging.h"
+#include "base/utils.h"
+
+CDataBlock::CDataBlock(long maxsize)
+:m_nMaxSize(maxsize)
+,m_nCurSize(0)
+,m_bInitData(false)
+{
+	m_pBase = new uchar[maxsize];
+    CHECK(NULL != m_pBase) << "在函数CDataBlock::CDataBlock(...),分配内存出错。";
+}
+
+CDataBlock::~CDataBlock(void)
+{
+	SAFE_DELETE_ARRAY(m_pBase);
+}
+
+void CDataBlock::SetCurSize(long lSize)
+{
+	m_nCurSize = (lSize > 0 && lSize <= m_nMaxSize) ? lSize : 0;
+}
---
+++ src/io/DataBlockAllocator.h
@@ -0,0 +1,77 @@
+//=============================================================================
+/**
+ *  file: DataBlockAllocator.h
+ *
+ *  Brief:固定大小的内存快分配器
+ *
+ *  Authtor:wangqiao
+ *	
+ *	Date:2007-6-13
+ */
+//=============================================================================
+
+
+#pragma once
+
+#include <windows.h>
+#include <map>
+#include <list>
+#include "base/DefType.h"
+
+class CDataBlock;
+class CGUID;
+
+struct tagDBOpParam
+{
+	//当前读写数据块编号
+	ulong	nCurNum;
+	//当前读写数据块的大小
+	ulong	nCurDBSize;
+	//当前读写数据的位置
+	ulong	nCurPos;
+	//当前读写数据块的指针
+	uchar	*pDBPtr;
+};
+
+class CDataBlockAllocator
+{
+public:
+	CDataBlockAllocator(int nDBNum,int nDBSize,int bIniShareDB = false);
+public:
+	~CDataBlockAllocator(void);
+
+private:
+	typedef std::list<CDataBlock*>	FreeDataBlocks;
+    typedef std::list<CDataBlock*>::iterator itFreeDB;
+
+	FreeDataBlocks m_FreeDataBlocks;
+	CRITICAL_SECTION m_CSFreeDB;
+	int	m_nMaxDBNum;
+	const int	m_nDBSize;
+	//用在逻辑层临时使用的数据块
+	//个数不多，使用较大的数据块来满足各种大小的需求
+    typedef std::multimap<long, uchar*>	MMapShareDB;
+    typedef std::pair <long, uchar*> ShareDBPair;
+    typedef std::multimap<long, uchar*>::iterator itShareDB;
+	MMapShareDB m_ShareDataBlocks;
+
+    typedef std::map<uchar*, long>	mapAllocedRecord;
+    typedef std::map<uchar*, long>::iterator itAllocR;
+	mapAllocedRecord	m_AllocedShareDB;
+	//允许的最大共享数据块个数
+    std::map<long, long>	m_MapTest;
+private:
+	void Initial(int bIniShareDB = false);
+	void Release();
+
+public:
+	CDataBlock*	AllocDB(long lTestFlag=0);
+	void FreeDB(CDataBlock*);
+
+	uchar* AllockShareDB(long lSize);
+	void FreeShareDB(uchar* pData);
+
+	void PutAllocInfo(const char* pszFileName);
+};
+
+
